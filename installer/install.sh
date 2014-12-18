2c49870544ede23606cdd2cebeaa2c0ee35bbe36
Renat <rzhilkibaev@gmail.com>
2014-12-18 12:59:26 -0800

    [installer] Run playbook

--- installer/install.sh
+++ installer/install.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
-echo "Hi!"
+
+cd /tmp
+
+# install ansible
 apt-get install software-properties-common -y
 apt-add-repository ppa:ansible/ansible -y
 apt-get update
 apt-get install ansible -y
+
+# install ansible roles
+ansible-galaxy install smola.java
+ansible-galaxy install groover.maven
+ansible-galaxy install kost.virtualbox
+
+# install git
+apt-get install git -y
+
+# get playbook
+git git@github.com:rzhilkibaev/dev-env.git
+
+# run playbook
+cd dev-env/ansible-playbook
+
+ansible-playbook -i tests/inventory site.yml --connection=local --sudo
+
