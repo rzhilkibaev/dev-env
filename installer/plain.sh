e472d0d09ce41d128518b010cd687d37e6c69329
Renat <rzhilkibaev@gmail.com>
2015-01-05 17:23:55 -0800

    Add plain bash script installer

---
+++ installer/plain.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# this is plain bash script installer, it doesn't use ansible.
+
+set WORKING_DIRECTORY=/var/tmp/dev-env-plain-installer
+set DOWNLOAD_CACHE_DIRECTORY=$WORKING_DIRECTORY/cache
+set LOG_FILE=$WORKING_DIRECTORY/log.txt
+set PACKAGES=virtualbox-4.3 dkms git subversion mercurial ant vim tree ansible oracle-java7-installer oracle-java8-installer oracle-java8-set-default
+
+mkdir -p $WORKING_DIRECTORY
+
+echo "Adding ppa repositories"
+apt-add-repository ppa:ansible/ansible -y >> $LOG_FILE
+apt-add-repository ppa:webupd8team/java -y >> $LOG_FILE
+grep -q -F 'virtualbox' /etc/apt/sources.list || echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add - >> $LOG_FILE
+apt-get update >> $LOG_FILE
+
+echo "Installing"
+
+apt-get install $PACKAGES -y >> $LOG_FILE
+
+
