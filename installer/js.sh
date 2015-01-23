68dd61d5f75688a0ae70a80449580729fe4dce3f
Renat <rzhilkibaev@gmail.com>
2015-01-22 17:21:06 -0800

    Uncomment common installer

--- installer/js.sh
+++ installer/js.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#https://github.com/rzhilkibaev/dev-env/raw/master/installer/plain.sh | sudo bash
+https://github.com/rzhilkibaev/dev-env/raw/master/installer/plain.sh | sudo bash
 
 
 git clone git@github.com:rzhilkibaev/jst.git ~/jst
