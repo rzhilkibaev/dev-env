1e22130177599774a7c9cd876007bd565280f40a
Renat <rzhilkibaev@gmail.com>
2015-04-22 14:29:41 -0700

    Use spaces for tabs

--- installer/install.sh
+++ installer/install.sh
@@ -109,38 +109,28 @@ EOL
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.subversion
 
 # Vim
-git clone https://github.com/gmarik/Vundle.vim.git $USERNAME/.vim/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git /home/$USERNAME/.vim/bundle/Vundle.vim
 
 cat > /home/$USERNAME/.vimrc <<"EOL"
 set nocompatible                                        " not compatible with vi, required by Vundle
 filetype off                                            " required by Vundle
 
-" set the runtime path to include Vundle and initialize
+" set the runtime path to include Vundle
 set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
 
-" let Vundle manage Vundle, required by Vundle
+call vundle#begin()
 Plugin 'gmarik/Vundle.vim'
 Plugin 'chase/vim-ansible-yaml'
+call vundle#end()
 
-" All of your Plugins must be added before the following line
-call vundle#end()            " required
-
-filetype plugin indent on    " required
-" To ignore plugin indent changes, instead use:
-"filetype plugin on
-"
-" Brief help
-" :PluginList       - lists configured plugins
-" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
-" :PluginSearch foo - searches for foo; append `!` to refresh local cache
-" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
-"
-" see :h vundle for more details or wiki for FAQ
 " Put your non-Plugin stuff after this line
 
+filetype plugin indent on
+
 :set nowrap
-:set tabstop=4
+:set tabstop=4 shiftwidth=4 expandtab
+:set hidden
+:set wildmenu
 
 "XML folding
 let g:xml_syntax_folding=1
