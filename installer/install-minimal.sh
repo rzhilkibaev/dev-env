13fa31e65120f1fa351691a52fbd4483c292a1b8
Renat <rzhilkibaev@gmail.com>
2015-05-24 21:50:35 -0700

    fix vim terminating the installation process
    vim terminates the installation process with 'input is not from a terminal' message

--- installer/install-minimal.sh
+++ installer/install-minimal.sh
@@ -105,7 +105,7 @@ autocmd FileType xml setlocal foldmethod=syntax
 :hi CursorColumn cterm=NONE ctermbg=black
 EOL
 chown $USERNAME:$USERNAME /home/$USERNAME/.vimrc
-vim +PluginInstall +qall
+vim -E -u NONE -S /home/$USERNAME/ +PluginInstall +qall
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.vim
 
 # Bash
