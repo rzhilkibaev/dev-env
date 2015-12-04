384b5897568ef83c7a3500642411b64f2876fb25
Renat <rzhilkibaev@gmail.com>
2015-12-04 11:02:31 -0800

    Install cifs-utils to mount windows shares

--- installer/install.sh
+++ installer/install.sh
@@ -85,7 +85,7 @@ function setup_container() {
 
     # Install
     apt-get update && apt-get install -y \
-        git subversion vim tree ansible curl python-pip python3-pip sshfs \
+        git subversion vim tree ansible curl python-pip python3-pip sshfs cifs-utils \
         oracle-java7-installer oracle-java8-installer oracle-java8-set-default ant ant-contrib
 
     # Install Maven
