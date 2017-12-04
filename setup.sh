6af4b3d930e3246c5acb605d0cd9e53fc15db5a0
Renat <rzhilkibaev@gmail.com>
2017-12-03 21:11:05 -0800

    Log tool name

--- setup.sh
+++ setup.sh
@@ -8,21 +8,21 @@
 main() {
 
     TOOL_NAMES=()
-    TOOL_NAMES+=(wget curl zip unzip git) # these are used by the other installers
+    #TOOL_NAMES+=(wget curl zip unzip git) # these are used by the other installers
     #TOOL_NAMES+=(python-pip python3-pip)
     #TOOL_NAMES+=(ansible vagrant terraform)
     #TOOL_NAMES+=(sdkman) # sdkman can be used to install maven, ant, gradle, java...
-    TOOL_NAMES+=(n) # n can be used to install nodejs, npm...
+    #TOOL_NAMES+=(n) # n can be used to install nodejs, npm...
     #TOOL_NAMES+=(nvim)
     #TOOL_NAMES+=(shell)
-    #TOOL_NAMES+=(docker)
+    TOOL_NAMES+=(docker)
     #TOOL_NAMES+=(sshfs cifs-utils)
     #TOOL_NAMES+=(gnome-terminal)
     #TOOL_NAMES+=(tree vifm subversion ag)
     #TOOL_NAMES+=(misc)
 
     export SETUP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    echo SETUP_HOME=$SETUP_HOME
+    #echo SETUP_HOME=$SETUP_HOME
 
     # make the name of the init script available to the custom installers
     export INIT_SCRIPT=$SETUP_HOME/setup.d/_init
@@ -31,6 +31,8 @@ main() {
 
     for TOOL_NAME in ${TOOL_NAMES[@]}; do
         echo "Installing $TOOL_NAME"
+        echo "" >> setup.log
+        echo "========================== Installing $TOOL_NAME" >> setup.log
         # use custom installer if exists
         if [ -f $SETUP_HOME/setup.d/$TOOL_NAME ]; then
             $SETUP_HOME/setup.d/$TOOL_NAME &>> setup.log
@@ -38,15 +40,9 @@ main() {
             $SETUP_HOME/setup.d/install_package $TOOL_NAME &>> setup.log
         fi
         if [ $? -ne 0 ]; then
-            echo "Error occured while installing $TOOL_NAME"
-            echo "START setup.log"
-            cat setup.log
-            echo "END of setup.log"
-            echo
+            echo "Error occured while installing $TOOL_NAME, see setup.log"
         fi
     done
-
-    #cat setup.log
 
     echo Done
 }
